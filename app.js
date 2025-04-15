726fd2c1f5aad929376fffe1951642432c0d0b21
adams <adamskiptalam0@gmail.com>
2025-04-15 15:41:32 +0300

    First complete homepage layout

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+//render views and assets
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
 app.listen(PORT, (req, res) => {
 	console.log(`listening at http://localhost:${PORT}`);
 });
